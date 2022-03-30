9dad5e3c440c7f404f1dc3434ad11ab6b125b683
Alex <44911042+AlexRedBlue@users.noreply.github.com>
2022-03-30 15:56:22 -0400

    TFMI_App is working

--- TFMI_App.py
+++ TFMI_App.py
@@ -43,6 +43,7 @@ def isStrInt(text):
     
 class TFdata:
     def __init__(self, TF_name, save_directory, drive):
+        self.current_working_dir = os.getcwd()
         self.TF_name = TF_name
         self.save_directory = save_directory
         self.get_today()
@@ -94,24 +95,28 @@ class TFdata:
         
         
     def getfilename(self, directory, file_tag, num=0):
-        fname = r"{}\{}__{}.dat".format(directory, file_tag, num)
+        fname = self.working_dir+r"\{}\{}__{}.dat".format(directory, file_tag, num)
+        if os.path.isdir(self.working_dir+"\\"+directory) == False:
+            os.makedirs(directory)
         if os.path.isfile(fname):
-            return fname
-        return self.getfilename(directory, file_tag, num+1)
+            return self.getfilename(directory, file_tag, num+1)
+        return fname
         
     def save_fits(self):
-        fname = self.getfilename(self.save_directory, "{}_{}".format(self.TF_name, self.today))
-        if type(self.header) == None:
-            np.savetxt(fname, self.fits, delimiter='\t')
-        else:
-            np.savetxt(fname, self.fits, header=self.header, delimiter='\t')
+        if np.array(self.fits).ndim > 1:
+            fname = self.getfilename(self.save_directory, "{}_fits_{}".format(self.TF_name, self.today))
+            if self.header is not None:
+                np.savetxt(fname, np.array(self.fits), header=self.header, delimiter='\t')
+            else:
+                np.savetxt(fname, np.array(self.fits), delimiter='\t')
             
-    def save_sweep(self, fname):
-        fname = self.getfilename(self.save_directory, "\sweeps_{}\{}_{}".format(self.today, self.TF_name, self.today))
-        if type(self.sweep_header) == None:
-            np.savetxt(fname, self.sweep, delimiter='\t')
+    def save_sweep(self):
+        fname = self.getfilename(self.save_directory+"\sweeps_{}".format(self.today), "{}_{}".format(self.TF_name, self.today))
+        if self.sweep_header is not None:
+            np.savetxt(fname, np.array(self.sweep), header=self.sweep_header, delimiter='\t')
         else:
-            np.savetxt(fname, self.sweep, header=self.sweep_header, delimiter='\t')
+            np.savetxt(fname, np.array(self.sweep), delimiter='\t')
+            
 
 class tkApp(tk.Tk):
     
@@ -144,19 +149,23 @@ class tkApp(tk.Tk):
         
         self.wm_title("Freq Sweep Alpha")
         
-        self.top = tk.Frame(self)
-        self.bottom2 = tk.Frame(self)
-        self.bottom1 = tk.Frame(self)
-        self.top.pack(side=tk.TOP)
-        self.bottom2.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-        self.bottom1.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.row1 = tk.Frame(self)
+        self.row1.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.row2 = tk.Frame(self)
+        self.row2.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.row3 = tk.Frame(self)
+        self.row3.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        # self.bottom1 = tk.Frame(self)
+        # self.bottom2.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        # self.bottom1.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
         
         self.fig = Figure(figsize=(8, 6), dpi=100)
         self.ax = self.fig.add_subplot(2,1,1)
         self.ay = self.fig.add_subplot(2,1,2)
         
         
-        self.data = TFdata(TF_name="test", save_directory=r"data", drive = 1)
+        self.TFdata = TFdata(TF_name="test", save_directory="data", drive = 1)
         self.params = {
                         "Num Pts": int(self.config["Frequency Sweep Settings"]["Num Pts"]),
                         "End Frequency": float(self.config["Frequency Sweep Settings"]["End Frequency"]),
@@ -164,17 +173,19 @@ class tkApp(tk.Tk):
                       }
         
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)  # A tk.DrawingArea.
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.canvas.get_tk_widget().pack(in_=self.row1, side=tk.TOP, fill=tk.BOTH, expand=1)
         
         self.toolbar = NavigationToolbar2Tk(self.canvas, self)
         self.toolbar.update()
-        self.canvas.get_tk_widget().pack(in_=self.top, fill=tk.BOTH, expand=1)
+        self.canvas.get_tk_widget().pack(in_=self.row1, side=tk.TOP, fill=tk.BOTH, expand=1)
         
         self.canvas.mpl_connect("key_press_event", self.on_key_press)
         
         
+        
+        
         self.start_button = tk.Button(master=self, text="Start", command=self.start)
-        self.start_button.pack(in_=self.bottom2, side=tk.LEFT, padx=5, pady=5)
+        self.start_button.pack(in_=self.row3, side=tk.LEFT, padx=5, pady=5)
         
         
         # labels & Text Boxes
@@ -182,33 +193,44 @@ class tkApp(tk.Tk):
         self.entry_list = []
         for idx, (key, val) in enumerate(self.params.items()):
             
-            self.label_list.append(tk.Label(self.top, text = key+" "+str(val)))
-            self.label_list[-1].pack(in_=self.top,side=tk.RIGHT)
+            self.label_list.append(tk.Label(self.row2, text = key+" "+str(val)))
+            self.label_list[-1].pack(in_=self.row2,side=tk.RIGHT)
             
-            self.entry_list.append(tk.Entry(self.top))
-            self.entry_list[-1].pack(in_=self.top, side=tk.RIGHT)
+            self.entry_list.append(tk.Entry(self.row2, width=10))
+            self.entry_list[-1].pack(in_=self.row2, side=tk.RIGHT)
 
         
         self.update_button = tk.Button(master=self, text="Update Params", command=self.update_sweep_params)
-        self.update_button.pack(in_=self.bottom2, side=tk.RIGHT)
+        self.update_button.pack(in_=self.row3, side=tk.RIGHT)
         
         self.fitBool = tk.IntVar()
-        self.fitBool.set(0)
+        self.fitBool.set(1)
         self.fit_check = tk.Checkbutton(master=self, text="Fit", variable=self.fitBool, onvalue=1, offvalue=0)
-        self.fit_check.pack(in_=self.bottom1, side=tk.LEFT, padx=5, pady=5)
+        self.fit_check.pack(in_=self.row3, side=tk.LEFT, padx=5, pady=5)
         
         self.trackBool = tk.IntVar()
         self.trackBool.set(0)
         self.track_check = tk.Checkbutton(master=self, text="Track", variable=self.trackBool, onvalue=1, offvalue=0)
-        self.track_check.pack(in_=self.bottom1, side=tk.LEFT, padx=5, pady=5)
+        self.track_check.pack(in_=self.row3, side=tk.LEFT, padx=5, pady=5)
         
         self.showGraph = tk.StringVar(self)
         self.showGraph.set("Frequency Sweep") # default value
         self.showGraph_Options = tk.OptionMenu(self, self.showGraph, "Frequency Sweep", "Fit Details", command=self.switchGraph)
-        self.showGraph_Options.pack(in_=self.bottom1, side=tk.LEFT, padx=5, pady=5)
+        self.showGraph_Options.pack(in_=self.row3, side=tk.LEFT, padx=5, pady=5)
         
         self.settings_button = tk.Button(master=self, text="Settings", command=self.settingsWindow)
-        self.settings_button.pack(in_=self.bottom2, side=tk.RIGHT, padx=5, pady=5)
+        self.settings_button.pack(in_=self.row3, side=tk.RIGHT, padx=5, pady=5)
+        
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def on_closing(self):
+        self.update_idletasks()
+        self.update()
+        if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.run = False
+            self.TFdata.save_fits()
+            self.quit()
+            self.destroy()
 
     def initConfig(self):
         self.config.add_section('Frequency Sweep Settings')
@@ -230,8 +252,6 @@ class tkApp(tk.Tk):
         
         with open('TFMI_Config.cfg', 'w') as output:
             self.config.write(output)
-        
-
     
     def updateConfig(self, section, key, value):
     #Update config using section key and the value to change
@@ -243,6 +263,7 @@ class tkApp(tk.Tk):
             
     def settingsWindow(self):
         sWin = tk.Toplevel(self)
+        
         sWin.geometry("480x360")
         sWin.title("Settings")
         
@@ -278,7 +299,7 @@ class tkApp(tk.Tk):
         close_button = tk.Button(sWin, text='Quit', command=lambda:close_win(sWin))
         close_button.place(anchor="se", x=465, y=345)
 
-        sWin.mainloop()
+        self.wait_window(self)
         
     def save_settings(self):
         
@@ -316,7 +337,6 @@ class tkApp(tk.Tk):
                     self.entry_list[idx].delete(0, 'end')
             except Exception as e:
                 print(e)
-        self.mainloop()
     
 
     def _quit(self):
@@ -325,64 +345,72 @@ class tkApp(tk.Tk):
         self.quit()     # stops mainloop
         self.destroy()  # this is necessary on Windows to prevent
                         # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+        self.TFdata.save_fits()
     
     def tracking(self):
         # TODO
         # Everything
         pass
     
-    def switchGraph(self):
-        if self.showGraph.get() == "Frequency Sweep":
+    def switchGraph(self, event):
+        if event == "Frequency Sweep":
             self.graph_sweep()
-        if self.showGraph.get() == "Fit Details":
+        if event == "Fit Details":
             self.graph_fits() 
+        self.update_idletasks()
+        self.update()
     
     def sweep(self):
         self.TFdata.reset_sweep()
         frequencies = np.linspace(self.params["Start Frequency"], self.params["End Frequency"], self.params["Num Pts"])
         self.gen.Set_Frequency(self.params["Start Frequency"])
-        time.sleep(3)
+        self.after(3000)
         for idx, f in enumerate(frequencies):
             self.gen.Set_Frequency(f)
-            time.sleep(1)
+            self.after(1000)
             Vx, Vy = self.lockin.Read_XY()
-            self.TFdata.append([time.time(), f, Vx, Vy])
+            self.TFdata.append_sweep([time.time(), f, Vx, Vy])
             if self.showGraph.get() == "Frequency Sweep":
                 self.graph_sweep()
+            self.update_idletasks()
+            self.update()
+                
 
     def graph_sweep(self):
         self.ax.clear()
         self.ay.clear()
-        self.ax.plot(np.asarray(self.TFdata.sweep)[:, 1], np.asarray(self.TFdata.sweep)[:, 2])
-        self.ay.plot(np.asarray(self.TFdata.sweep)[:, 1], np.asarray(self.TFdata.sweep)[:, 3])
+        if np.asarray(self.TFdata.sweep).ndim > 1:
+            self.ax.plot(np.asarray(self.TFdata.sweep)[:, 1], np.asarray(self.TFdata.sweep)[:, 2])
+            self.ay.plot(np.asarray(self.TFdata.sweep)[:, 1], np.asarray(self.TFdata.sweep)[:, 3])
         self.ax.set_title("Frequency Sweep")
         self.ax.set_ylabel("I")
         self.ay.set_ylabel("I")
         self.ay.set_xlabel("f, hz")
         self.canvas.draw()
-        self.update_idletasks()
-        self.update()
+        
         
     def graph_fits(self):
+        
         self.ax.clear()
         self.ay.clear()
-        self.ax.plot(self.TFdata.saved_fits[:, 0]-self.TFdata.timestamp_today, self.TFdata.saved_fits[:, 4])
-        self.ay.plot(self.TFdata.saved_fits[:, 0]-self.TFdata.timestamp_today, self.TFdata.saved_fits[:, 2])
-        self.ax.plot(np.asarray(self.TFdata.fits)[:, 0]-self.TFdata.timestamp_today, np.asarray(self.TFdata.fits[:, 4]))
-        self.ay.plot(np.asarray(self.TFdata.fits)[:, 0]-self.TFdata.timestamp_today, np.asarray(self.TFdata.fits[:, 2]))
+        if self.TFdata.saved_fits.ndim > 1:
+            self.ax.plot(self.TFdata.saved_fits[:, 0]-self.TFdata.timestamp_today, self.TFdata.saved_fits[:, 4])
+            self.ay.plot(self.TFdata.saved_fits[:, 0]-self.TFdata.timestamp_today, self.TFdata.saved_fits[:, 2])
+        if np.asarray(self.TFdata.fits).ndim > 1:
+            self.ax.plot(np.asarray(self.TFdata.fits)[:, 0]-self.TFdata.timestamp_today, np.asarray(self.TFdata.fits)[:, 4])
+            self.ay.plot(np.asarray(self.TFdata.fits)[:, 0]-self.TFdata.timestamp_today, np.asarray(self.TFdata.fits)[:, 2])
         self.ax.set_title("Fit Values")
         self.ax.set_ylabel("Width, hz")
-        self.ay.set_ylabel("time")
+        self.ay.set_ylabel("$f_0$, hz")
         self.ay.set_xlabel("time")
         self.canvas.draw()
-        self.update_idletasks()
-        self.update()
+        
 
     def start(self):
         self.start_button.config(text="Stop", command=self.stop)
-        
         # Main Loop
-        while True:
+        self.run = True
+        while self.run:
             try:
                 self.sweep()
                 self.TFdata.save_sweep()
@@ -391,26 +419,19 @@ class tkApp(tk.Tk):
                     if self.showGraph.get() == "Fit Details":
                         self.graph_fits()
                     if (time.time()-self.TFdata.last_save)/60 > self.save_interval:
-                        self.save_fits()
+                        self.TFdata.save_fits()
             except:
-                if np.array(self.TFdata.fits).ndim > 1:
+                if self.run:
                     self.TFdata.save_fits()
+                    self.run = False
+                    self.quit()
+                    self.destroy()
                     
         
     def stop(self):
         self.start_button.config(text="Start Sweep", command=self.start)
         self.mainloop()
-        
-    def save_sweep(self):
-        file = tk.filedialog.asksaveasfile(initialdir=self.config["Frequency Sweep Settings"]["Save Directory"])
-        if type(file) != None and self.data != []:
-            np.savetxt(file.name, self.data, delimiter='\t', header='Time\tFreq\tX\tY')
-            self.updateConfig("Frequency Sweep Settings", "Save Directory", file.name)
-            self.save_label.config(text=file.name)
-        file.close()
-        self.mainloop()
-        
-        
+
         
 if __name__ == "__main__":
 
