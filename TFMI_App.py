7c7d287fa9cfb2c56a36a7fc2fd052d2602734ff
Alex <44911042+AlexRedBlue@users.noreply.github.com>
2022-03-31 16:50:24 -0400

    Updating Settings in Config
    More settings save and changable

--- TFMI_App.py
+++ TFMI_App.py
@@ -42,12 +42,13 @@ def isStrInt(text):
         return False
     
 class TFdata:
-    def __init__(self, TF_name, save_directory, drive):
+    def __init__(self, TF_name, save_directory):
         self.current_working_dir = os.getcwd()
         self.TF_name = TF_name
         self.save_directory = save_directory
         self.get_today()
-        self.drive = drive
+        self.set_drive(1)
+        self.set_current_amp(1)
         self.header = None
         self.sweep_header = None
         self.reset_sweep()
@@ -60,6 +61,12 @@ class TFdata:
         self.today = datetime(year=now.year, month=now.month, day=now.day)
         self.timestamp_today = time.mktime(self.today.timetuple())
         self.today = self.today.strftime("%Y-%m-%d")
+        
+    def set_drive(self, value):
+        self.drive = value
+        
+    def set_current_amp(self, value):
+        self.current_amp=value
         
     def reset_save_time(self):
         self.last_save = time.time()
@@ -99,14 +106,14 @@ class TFdata:
             guess = [(self.sweep[-1, 1]+self.sweep[0, 1])/2, 0, (self.sweep[-1, 1]-self.sweep[0, 1])/8, 0, 0, 0, 0]
             xfit, xflag = LF.Lorentz_Fit_X_quad(self.sweep[:, 1], self.sweep[:, 2], guess)
             xfit = np.abs(xfit)
-            print(xfit)
+            # print(xfit)
             yfit, yflag = LF.Lorentz_Fit_X_quad(self.sweep[:, 1], self.sweep[:, 3], guess)
             yfit = np.abs(yfit)
         self.append_fits([self.sweep[:, 0].mean(), self.drive, *xfit, *yfit])
         
         
     def getfilename(self, directory, file_tag, num=0):
-        fname = self.current_working_dir+r"\{}\{}__{}.dat".format(directory, file_tag, num)
+        fname = self.current_working_dir+r"\data\{}\{}__{}.dat".format(directory, file_tag, num)
         if os.path.isdir(self.current_working_dir+"\\"+directory) == False:
             os.makedirs(directory)
         if os.path.isfile(fname):
@@ -174,14 +181,20 @@ class tkApp(tk.Tk):
         self.fig = Figure(figsize=(8, 6), dpi=100)
         self.ax = self.fig.add_subplot(2,1,1)
         self.ay = self.fig.add_subplot(2,1,2)
+
         
+        self.TFdata = TFdata(TF_name=self.config["Monitor Save Settings"]["Tuning Fork Name"], 
+                             save_directory=self.config["Monitor Save Settings"]["Save Folder"])
+        self.TFdata.set_drive(float(self.config["Frequency Sweep Settings"]["Drive"]))
+        self.TFdata.set_current_amp(float(self.config["Frequency Sweep Settings"]["Current Amp"]))
         
-        self.TFdata = TFdata(TF_name="test", save_directory="data", drive = 1)
         self.params = {
                         "Num Pts": int(self.config["Frequency Sweep Settings"]["Num Pts"]),
                         "End Frequency": float(self.config["Frequency Sweep Settings"]["End Frequency"]),
                         "Start Frequency": float(self.config["Frequency Sweep Settings"]["Start Frequency"])
                       }
+        
+        self.wait_time = int(float(self.config["Frequency Sweep Settings"]["Wait Time"])*1000) # wait time in ms
         
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)  # A tk.DrawingArea.
         self.canvas.get_tk_widget().pack(in_=self.row1, side=tk.TOP, fill=tk.BOTH, expand=1)
@@ -215,13 +228,13 @@ class tkApp(tk.Tk):
         self.update_button.pack(in_=self.row3, side=tk.RIGHT)
         
         self.fitBool = tk.IntVar()
-        self.fitBool.set(1)
-        self.fit_check = tk.Checkbutton(master=self, text="Fit", variable=self.fitBool, onvalue=1, offvalue=0)
+        self.fitBool.set(int(self.config["Monitor Checkbox Settings"]["Fitting"]))
+        self.fit_check = tk.Checkbutton(master=self, text="Fit", variable=self.fitBool, onvalue=1, offvalue=0, command=self.update_checkbox_config)
         self.fit_check.pack(in_=self.row3, side=tk.LEFT, padx=5, pady=5)
         
         self.trackBool = tk.IntVar()
-        self.trackBool.set(0)
-        self.track_check = tk.Checkbutton(master=self, text="Track", variable=self.trackBool, onvalue=1, offvalue=0)
+        self.trackBool.set(int(self.config["Monitor Checkbox Settings"]["Tracking"]))
+        self.track_check = tk.Checkbutton(master=self, text="Track", variable=self.trackBool, onvalue=1, offvalue=0, command=self.update_checkbox_config)
         self.track_check.pack(in_=self.row3, side=tk.LEFT, padx=5, pady=5)
         
         self.showGraph = tk.StringVar(self)
@@ -248,18 +261,24 @@ class tkApp(tk.Tk):
         self.config.set('Frequency Sweep Settings', 'Start Frequency', '32700')
         self.config.set('Frequency Sweep Settings', 'End Frequency', '32900')
         self.config.set('Frequency Sweep Settings', 'Num Pts', '100')
+        self.config.set('Frequency Sweep Settings', 'Wait Time', '1')
+        self.config.set('Frequency Sweep Settings', 'Drive', '1')
+        self.config.set('Frequency Sweep Settings', 'Current Amp', '10000')
         self.config.set('Frequency Sweep Settings', 'Save Directory', r"C:\Users\physics-svc-mkdata\Documents\Data\TuningForkThermometer")
         
-        # self.config.add_section("Instrument Settings")
-        # self.config.set("Instrument Settings", "Lock-in Model", "Test")
-        # self.config.set("Instrument Settings", "Lock-in GPIB", "25")
-        # self.config.set("Instrument Settings", "Signal-Gen Model", "Test")
-        # self.config.set("Instrument Settings", "Signal-Gen GPIB", "24")
-        
+        self.config.add_section("Instrument Settings")
         self.config.set("Instrument Settings", "Lock-in Model", "LI 5640")
         self.config.set("Instrument Settings", "Lock-in GPIB", "25")
         self.config.set("Instrument Settings", "Signal-Gen Model", "Keysight")
         self.config.set("Instrument Settings", "Signal-Gen GPIB", "24")
+        
+        self.config.add_section("Monitor Save Settings")
+        self.config.set("Monitor Save Settings", "Save Folder", "Testing")
+        self.config.set("Monitor Save Settings", "Tuning Fork Name", "TestTF")
+        
+        self.config.add_section("Monitor Checkbox Settings")
+        self.config.set("Monitor Checkbox Settings", "Fitting", "1")
+        self.config.set("Monitor Checkbox Settings", "Tracking", "0")
         
         with open('TFMI_Config.cfg', 'w') as output:
             self.config.write(output)
@@ -272,38 +291,67 @@ class tkApp(tk.Tk):
         with open('TFMI_Config.cfg', 'w') as output:
             self.config.write(output)
             
+    def update_checkbox_config(self):
+        self.updateConfig("Monitor Checkbox Settings", "Fitting", self.fitBool.get())
+        self.updateConfig("Monitor Checkbox Settings", "Tracking", self.trackBool.get())
+            
     def settingsWindow(self):
         sWin = tk.Toplevel(self)
         
         sWin.geometry("480x360")
         sWin.title("Settings")
         
+        # GPIB Settings
         GPIB_Label = tk.Label(sWin, text="GPIB")
-        GPIB_Label.place(x=95, y=15)
+        GPIB_Label.place(x=90, y=15)
         
         Lockin_Label = tk.Label(sWin, text="Lock In")
         Lockin_Label.place(x=15, y=35)
         
-        self.SignalGen_Label = tk.Label(sWin, text="Signal Gen")
-        self.SignalGen_Label.place(x=15, y=75)
+        SignalGen_Label = tk.Label(sWin, text="Signal Gen")
+        SignalGen_Label.place(x=15, y=75)
         
-        self.Lockin_GPIB = tk.Entry(sWin)
+        self.Lockin_GPIB = tk.Entry(sWin, width=5)
         self.Lockin_GPIB.insert(0, self.config["Instrument Settings"]["Lock-in GPIB"])
         self.Lockin_GPIB.place(anchor="nw", x=90, y=35)
         
-        self.SignalGen_GPIB = tk.Entry(sWin)
+        self.SignalGen_GPIB = tk.Entry(sWin, width=5)
         self.SignalGen_GPIB.insert(0, self.config["Instrument Settings"]["Signal-Gen GPIB"])
         self.SignalGen_GPIB.place(anchor="nw", x=90, y=75)
         
         self.Lockin_Model = tk.StringVar(sWin)
         self.Lockin_Model.set(self.config["Instrument Settings"]["Lock-in Model"]) # default value
         self.L_Model_Options = tk.OptionMenu(sWin, self.Lockin_Model, "LI 5640", "SR 830")
-        self.L_Model_Options.place(anchor="nw", x=235, y=28)
+        self.L_Model_Options.place(anchor="nw", x=160, y=28)
         
         self.SignalGen_Model = tk.StringVar(sWin)
         self.SignalGen_Model.set(self.config["Instrument Settings"]["Signal-Gen Model"]) # default value
         self.SG_Model_Options = tk.OptionMenu(sWin, self.SignalGen_Model, "Keysight", "Agilent")
-        self.SG_Model_Options.place(anchor="nw", x=235, y=68)
+        self.SG_Model_Options.place(anchor="nw", x=160, y=68)
+
+        # Save Folder
+        self.Save_Folder = tk.Entry(sWin, width=12)
+        self.Save_Folder.insert(0, self.config["Monitor Save Settings"]["Save Folder"])
+        self.Save_Folder.place(anchor="nw", x=90, y=120)
+        
+        Lockin_Label = tk.Label(sWin, text="Save Folder")
+        Lockin_Label.place(x=15, y=120)
+        
+        # TF Name
+        self.TF_savename = tk.Entry(sWin, width=12)
+        self.TF_savename.insert(0, self.config["Monitor Save Settings"]["Tuning Fork Name"])
+        self.TF_savename.place(anchor="nw", x=90, y=150)
+        
+        Lockin_Label = tk.Label(sWin, text="TF Name")
+        Lockin_Label.place(x=15, y=150)
+        
+        next_fit_name = self.TFdata.getfilename(self.TFdata.save_directory, "{}_fits_{}".format(self.TFdata.TF_name, self.TFdata.today)).replace(self.TFdata.current_working_dir,'')
+        
+        self.fit_Label = tk.Label(sWin, text="Next Fit File:        "+next_fit_name)
+        self.fit_Label.place(x=15, y=180)
+        
+        self.sweep_label = tk.Label(sWin, text="Current Sweep Folder: "+"\data\{}\sweeps_{}".format(self.config["Monitor Save Settings"]["Save Folder"], self.TFdata.today))
+        self.sweep_label.place(x=15, y=210)
 
         save_button = tk.Button(sWin, text='Save', command=self.save_settings)
         save_button.place(anchor="sw", x=15, y=345)
@@ -332,6 +380,18 @@ class tkApp(tk.Tk):
             self.gen = AGILENT_SIGNAL_GEN.AGILENT_SIGNAL_GEN("GPIB0::"+self.config["Instrument Settings"]["Signal-Gen GPIB"]+"::INSTR")
         else:
             print("Invalid Signal-Gen Instrument Type: Reset Config File")
+        
+        if self.Save_Folder != self.config["Monitor Save Settings"]["Save Folder"]:
+            self.updateConfig("Monitor Save Settings", "Save Folder", self.Save_Folder.get())
+            self.TFdata.save_directory = self.Save_Folder.get()
+            next_fit_name = self.TFdata.getfilename(self.TFdata.save_directory, "{}_fits_{}".format(self.TFdata.TF_name, self.TFdata.today)).replace(self.TFdata.current_working_dir,'')
+            self.fit_Label.config(text="Next Fit File:        "+next_fit_name)
+        
+        if self.TF_savename != self.config["Monitor Save Settings"]["Tuning Fork Name"]:
+            self.updateConfig("Monitor Save Settings", "Tuning Fork Name", self.TF_savename.get())
+            self.TFdata.TF_name = self.TF_savename.get()
+            self.sweep_label.config(text="Current Sweep Folder: "+"\data\{}\sweeps_{}".format(self.TFdata.save_directory, self.TFdata.today))
+            
     
 
     def on_key_press(self, event):
@@ -369,23 +429,7 @@ class tkApp(tk.Tk):
         if event == "Fit Details":
             self.graph_fits() 
         self.update_idletasks()
-        self.update()
-    
-    def sweep(self):
-        self.TFdata.reset_sweep()
-        frequencies = np.linspace(self.params["Start Frequency"], self.params["End Frequency"], self.params["Num Pts"])
-        self.gen.Set_Frequency(self.params["Start Frequency"])
-        self.after(3000)
-        for idx, f in enumerate(frequencies):
-            self.gen.Set_Frequency(f)
-            self.after(1000)
-            Vx, Vy = self.lockin.Read_XY()
-            self.TFdata.append_sweep([time.time(), f, Vx, Vy])
-            if self.showGraph.get() == "Frequency Sweep":
-                self.graph_sweep()
-            self.update_idletasks()
-            self.update()
-                
+        self.update()       
 
     def graph_sweep(self):
         self.ax.clear()
@@ -416,6 +460,21 @@ class tkApp(tk.Tk):
         self.ay.set_xlabel("time")
         self.canvas.draw()
         
+        
+    def sweep(self):
+        self.TFdata.reset_sweep()
+        frequencies = np.linspace(self.params["Start Frequency"], self.params["End Frequency"], self.params["Num Pts"])
+        self.gen.Set_Frequency(self.params["Start Frequency"])
+        self.after(3*self.wait_time)
+        for idx, f in enumerate(frequencies):
+            self.gen.Set_Frequency(f)
+            self.after( self.wait_time)
+            Vx, Vy = self.lockin.Read_XY()
+            self.TFdata.append_sweep([time.time(), f, Vx, Vy])
+            if self.showGraph.get() == "Frequency Sweep":
+                self.graph_sweep()
+            self.update_idletasks()
+            self.update()
 
     def start(self):
         self.start_button.config(text="Stop", command=self.stop)
@@ -439,8 +498,11 @@ class tkApp(tk.Tk):
                 if self.run:
                     self.TFdata.save_fits()
                     self.run = False
-                    self.quit()
-                    self.destroy()
+                    try:
+                        self.quit()
+                        self.destroy()
+                    except Exception as e:
+                        print(e)
                     
         
     def stop(self):
