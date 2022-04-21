4a3aedcc4a0421f22c01fbe71656592de123424e
Alex <44911042+AlexRedBlue@users.noreply.github.com>
2022-04-21 13:27:45 -0400

    changes to info.ticks

--- functions/info.py
+++ functions/info.py
@@ -5,33 +5,34 @@ Created on Mon Apr 11 13:54:18 2022
 @author: physics-svc-mkdata
 """
 
-from datetime import timedelta
+from datetime import datetime
 import numpy as np
 
 def ticks_to_hours(graph, time_start, time_end):
     new_ticks = []
     xlabels = []
-    time_start, time_end = time_start/3600, time_end/3600
     # time if time length of data is < 30 minutes, use 7 HH:MM:SS; otherwise 9 HH:MM
-    if (time_end - time_start)*60 < 30:
+    if (time_end - time_start) < 30*60:
         ticks = np.linspace(time_start, time_end, 7)
-        for idx, val in enumerate(ticks):
-            dt = str(timedelta(seconds=int(val*3600)))
-            if dt not in xlabels:
-                xlabels.append(dt)
-                new_ticks.append(val*3600)
-    else:
+        for time in ticks:
+            digital_time = datetime.fromtimestamp(time).strftime("%H:%M:%S")
+            if digital_time not in xlabels:
+                xlabels.append(digital_time)
+                new_ticks.append(time)
+    elif (time_end - time_start) < 24*60*60:
         ticks = np.linspace(time_start, time_end, 9)
-        for idx, val in enumerate(ticks):
-            dt = str(timedelta(seconds=int(val*3600))).split(":")
-            try:
-                time = float(dt[0]) + float(dt[1])/60
-            except:
-                time = val
-            dt = dt[0]+":"+dt[1]
-            if dt not in xlabels:
-                xlabels.append(dt)
-                new_ticks.append(time*3600)
+        for time in ticks:
+            digital_time = datetime.fromtimestamp(time).strftime("%H:%M")
+            if digital_time not in xlabels:
+                xlabels.append(digital_time)
+                new_ticks.append(time)
+    else:
+        ticks = np.linspace(time_start, time_end, 7)
+        for time in ticks:
+            digital_time = datetime.fromtimestamp(time).strftime("%b %d %H:%M")
+            if digital_time not in xlabels:
+                xlabels.append(digital_time)
+                new_ticks.append(time)
 
     graph.set_xticks(new_ticks)
     graph.set_xticklabels(xlabels)
--- instruments/LAKESHORE.py
+++ instruments/LAKESHORE.py
@@ -49,4 +49,38 @@ class LS370:
     def Ask_Heater_Output(self, channel):
         return float(self.inst.query("HTR?").rstrip())
         
+class LS336:
+    
+    ID = ''
+    
+    def __init__(self, GPIB_Addr):
+        rm = pyvisa.ResourceManager()
+        self.inst = rm.open_resource(GPIB_Addr)
+        # self.inst.read_termination ='\r\n'
+        # self.inst.write_termination ='\r\n'
+        self.ID = self.inst.query("*IDN?")
+        # self.sens = self.sens_dict[int(self.inst.query("SENS?").rstrip())]
+        
+    def Read_R(self, channel=1):
+        return float(self.inst.query("RDGR? "+str(channel)).rstrip())
+        
+    def Ask_Scan_Channel(self):
+        return float(self.inst.query("SCAN?").rstrip())
+        
+    def Autoscan(self, channels=[1], wait_time=5):
+        measurements = []
+        for idx, channel in enumerate(channels):
+            self.inst.write("SCAN {:d},0".format(channel))
+            plt.pause(wait_time)
+            measurements.append([channel, self.Read_R(channel)])
+        return measurements
+        
+    def Scan(self, channel=1):
+        self.inst.write("SCAN "+str(channel))
+        
+    def Ask_Heater_Range(self, channel):
+        return float(self.inst.query("HTRRNG?").rstrip())
+    
+    def Ask_Heater_Output(self, channel):
+        return float(self.inst.query("HTR?").rstrip())
         
